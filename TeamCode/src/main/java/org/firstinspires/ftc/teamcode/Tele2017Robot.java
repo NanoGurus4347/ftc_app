4bbdaf550d0ad060f3a364237073dc98528b59ac
NanoGurus4347 <34872707+NanoGurus4347@users.noreply.github.com>
2017-12-26 20:43:55 -0500

    Update Tele2017Robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele2017Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele2017Robot.java
@@ -1,8 +1,266 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
 /**
  * Created by Rithv on 12/26/2017.
  */
 
 public class Tele2017Robot {
+  // Declare OpMode members.
+    //private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFront = null;
+    private DcMotor rightFront = null;
+    private DcMotor leftBack = null;
+    private DcMotor rightBack = null;
+    private DcMotor collection = null;
+    private DcMotor delivery = null;
+    private DcMotor flip = null;
+    private DcMotor slide = null;
+    private Servo clawGrab = null;
+    private Servo clawArm = null;
+    private Servo stopper = null;
+    private Servo autoJewel = null;
+    boolean reversed = false, check = false, open = true, scorePos = false, scoring = true;
+    int zero = 0;
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        leftFront  = hardwareMap.get(DcMotor.class, "ld1");
+        rightFront = hardwareMap.get(DcMotor.class, "rd1");
+        leftBack  = hardwareMap.get(DcMotor.class, "ld2");
+        rightBack = hardwareMap.get(DcMotor.class, "rd2");
+        collection = hardwareMap.get(DcMotor.class, "clt");
+        delivery = hardwareMap.get(DcMotor.class, "dlv");
+        flip = hardwareMap.get(DcMotor.class, "flp");
+        slide = hardwareMap.get(DcMotor.class, "sld");
+        clawArm = hardwareMap.get(Servo.class, "arm");
+        clawGrab = hardwareMap.get(Servo.class, "grab");
+        stopper = hardwareMap.get(Servo.class, "stp");
+        autoJewel = hardwareMap.get(Servo.class, "auto");
+
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        delivery.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        flip.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        collection.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+        clawGrab.setPosition(.9);
+        stopper.setPosition(.75);
+        autoJewel.setPosition(.2);
+
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        //runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        // Setup a variable for each drive wheel to save power level for telemetry
+        double left   = motorPower(gamepad1.left_stick_y);
+        double right  = motorPower(gamepad1.right_stick_y);
+        //imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+        telemetry.addData("mR1", rightFront.getCurrentPosition());
+        telemetry.addData("mR2", rightBack.getCurrentPosition());
+        telemetry.addData("mL1", leftFront.getCurrentPosition());
+        telemetry.addData("mL2", leftBack.getCurrentPosition());
+
+        if (reversed) {
+            if (gamepad2.left_bumper) {
+                rightFront.setPower(right / 3);
+                leftFront.setPower(left / 3);
+                rightBack.setPower(right / 3);
+                leftBack.setPower(left / 3);
+            } else {
+                rightFront.setPower(right);
+                leftFront.setPower(left);
+                rightBack.setPower(right);
+                leftBack.setPower(left);
+            }
+        } else if (!reversed) {
+            if (gamepad2.left_bumper) {
+                rightFront.setPower(-left / 3);
+                leftFront.setPower(-right / 3);
+                rightBack.setPower(-left / 3);
+                leftBack.setPower(-right / 3);
+            } else {
+                rightFront.setPower(-left);
+                leftFront.setPower(-right);
+                rightBack.setPower(-left);
+                leftBack.setPower(-right);
+            }
+        }
+
+        if (gamepad1.dpad_left) {
+            check = true;
+        }
+
+        if(check && (!gamepad1.dpad_left)) {
+            reversed = !reversed;
+            check = false;
+        }
+
+        if(gamepad1.right_trigger > .05) {
+            collection.setPower(-gamepad1.right_trigger/1.25);
+        } else if(gamepad1.left_trigger > .05) {
+            collection.setPower(gamepad1.left_trigger/1.25);
+        } else {
+            collection.setPower(0);
+        }
+
+        if(gamepad1.dpad_up) {
+            stopper.setPosition(.75);
+        } else if(gamepad1.dpad_down) {
+            stopper.setPosition(.4);
+        }
+
+        if(gamepad1.b) {
+            clawGrab.setPosition(.75);
+        } else if(gamepad1.y) {
+            clawGrab.setPosition(.3);
+        }
+
+        if(gamepad1.left_bumper) {
+            clawArm.setPosition(.3);
+        } else if (gamepad1.x) {
+            clawArm.setPosition(.875);
+        } else if(gamepad1.right_bumper) {
+            clawArm.setPosition(0.135);
+        }
+
+        if(gamepad1.start) {
+            zero = delivery.getCurrentPosition();
+        }
+
+        if(gamepad2.b) {
+            delivery.setPower(.35);
+            scorePos = false;
+        } else if(gamepad2.a && gamepad2.x) {
+            scorePos = true;//-440!!!
+        } else if(gamepad2.x) {
+            delivery.setPower(-.75);
+            //scorePos = false;
+        } else if(!scorePos){
+            delivery.setPower(0.0); //set position instead
+        }
+
+        if(scorePos && delivery.getCurrentPosition() > zero + -440*3) {
+            delivery.setPower(-.75);
+            flip.setPower(-.2);
+        } else {
+            //scoring = false;
+            scorePos = false;
+            //delivery.setPower(0.0);
+        }
+
+//        if(gamepad2.b) {
+//            delivery.setPower(.35);
+//            scoring = false;
+//        } else if(gamepad2.a && gamepad2.x) {
+//            scorePos = true;//-440!!!
+//        } else if(gamepad2.x) {
+//            delivery.setPower(-.75);
+//        } else if(!scorePos && !scoring){
+//            delivery.setPower(0);
+//        } else if(scoring) {
+//            delivery.setPower(-.2); //set position instead
+//        }
+//
+//        if(scorePos && delivery.getCurrentPosition() > zero + -450) {
+//            delivery.setPower(-.75);
+//            flip.setPower(-.2);
+//        } else if(Math.abs(delivery.getCurrentPosition() - (zero + -450)) < 10) {
+//            scorePos = false;
+//            scoring = true;
+//        } else {
+//            scoring = false;
+//            scorePos = false;
+//        }
+
+
+        telemetry.addData("dlv", delivery.getCurrentPosition());
+
+        if(gamepad2.right_bumper) {
+            flip.setPower(.4);
+        } else if(gamepad2.left_trigger > .3) {
+            flip.setPower(-gamepad2.left_trigger/2);
+        } else {
+            flip.setPower(0);
+        }
+
+        if(gamepad2.right_trigger > .05) {
+            slide.setPower(gamepad2.right_trigger);
+        } else if(gamepad2.left_bumper) {
+            slide.setPower(-0.5);
+        } else {
+            slide.setPower(0);
+        }
+
+
+        // Show the elapsed game time and wheel power.
+        telemetry.update();
+    }
+
+    public double motorPower(double joy1) {
+        double[] values = {0.0, 0.15, 0.35, 0.55, 0.65, 0.85, 1.0};
+        double sign;
+
+        if(joy1 < 0) {
+            sign = -1.0;
+        } else {
+            sign = 1.0;
+        }
+
+        if(Math.abs(joy1) < values[1]) {
+            return 0;
+        } else if(Math.abs(joy1) < values[2]) {
+            return sign*values[1];
+        } else if(Math.abs(joy1) < values[3]) {
+            return sign*values[2];
+        } else if(Math.abs(joy1) < values[4]) {
+            return sign*values[3];
+        } else if(Math.abs(joy1) < values[5]) {
+            return sign*values[4];
+        } else if(Math.abs(joy1) < values[6]) {
+            return sign*values[5];
+        } else {
+            return sign*1.0;
+        }
+
+
+    }
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+}
+
+  
 }
