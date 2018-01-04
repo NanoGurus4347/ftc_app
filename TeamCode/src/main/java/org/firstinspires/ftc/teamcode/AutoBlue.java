66fe0043aef8b91350514b3ba2388b5d4281d886
mgovilla <31316460+mgovilla@users.noreply.github.com>
2018-01-04 15:58:53 -0500

    Android Studio Git

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlue.java
@@ -3,25 +3,62 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 /**
- * Created by Rithv on 12/26/2017.
+ * Created by vaish on 11/14/2017.
  */
-
-@Autonomous
-public class AutoBlue extends Auto2017Methods {
+@Autonomous(name="Blue Auto Middle")
+public class AutoBlue extends Auto2017Methods{
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        //motors
         initRobot();
 
-//score jewel and drive into safe zone -- 45 pts
         waitForStart();
-
+        int vuMark = vuScoreia(4);
+        telemetry.addData("vuScoreia", vuMark);
+        telemetry.update();
+        sleep(1000);
         scoreJewelBlue();
+        sleep(1000);
+
+        setPower(.2);
+        while(line.blue() < 10 && opModeIsActive()) {
+            telemetry.addData("line", "not detected");
+            telemetry.update();
+        }
+        while(line.blue() > 5 && opModeIsActive()) {
+            telemetry.addData("line", "detected");
+            telemetry.update();
+        }
+        halt();
+
+        telemetry.addData("moveBack: ", vuMark*6);
+        sleep(2000);
+        //moveBackwardEncoders(6, .3);
+
+
+        turn90Encoders();
+
+        /*setPowerSides(-.4, -.2);
+        sleep(1000);
+        halt();*/
+
+        collection.setPower(1);
+        sleep(500);
+        collection.setPower(0);
+
+        moveBackwardEncoders(5, .3);
+
+        setPower(-.3);
+        sleep(1500);
+        halt();
+
+        moveBackwardEncoders(5, .3);
+
+        /*score*/
+        /*moveBack*/
 
         stop();
 
     }
-    
 }
